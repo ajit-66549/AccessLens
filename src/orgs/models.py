5e89cefd33ff5565ea3bec9e7f596499c737266e
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-01-07 01:05:24 -0600

    phase1: add organizations, memberships, and RBAC models

--- src/orgs/models.py
+++ src/orgs/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
 # Create your models here.
+class Organization(models.Model):
+    """
+    A workspace
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=80, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Membership(models.Model):
+    """
+    A user belonging to an organization
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="memberships",)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="memberships",)
+    
+    is_active = models.BooleanField(default=True)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "organization"],
+                name="unique_user_org_membership",
+            )
+        ]
+        
+    def __str__(self):
+        return f"{self.user.username} @ {self.organization.slug}"
--- src/rbac/models.py
+++ src/rbac/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+import uuid
+from orgs.models import Membership
 
 # Create your models here.
+class Role(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    key = models.SlugField(max_length=80, unique=True)
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+    
+    is_system = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.key
+    
+class RoleAssignment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    membership = models.ForeignKey(Membership, on_delete=models.CASCADE, related_name="role_assignments")
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name="assignments")
+    assigned_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["membership", "role"],
+                name="unique_role_per_membership",
+            )
+        ]
+    def __str__(self):
+        return f"{self.membership} -> {self.role.key}"
