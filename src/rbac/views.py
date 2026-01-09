de12e8d4b85f3c32831b4573fa4196cffb30fe41
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-01-08 18:48:56 -0600

    refactor: use get_membership_role_keys in views

--- src/rbac/views.py
+++ src/rbac/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .permissions import IsOrgAdminOrOwner
-from .services import membership_has_any_role
+from .services import get_membership_role_keys
 
 # Create your views here.
 class WhoAmIScopedView(APIView):
     permission_classes = [IsOrgAdminOrOwner]
     
     def get(self, request):
-        roles = sorted(list(membership_has_any_role(request.membership)))
+        roles = sorted(get_membership_role_keys(request.membership))
         return Response({
             "user": request.user.username,
             "org": request.org.slug,
+            "membership_id": request.membership.id,
             "roles": roles,
             "access": "admin_or_owner",
         })
