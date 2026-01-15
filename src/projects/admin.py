37df9a97541b43fe6ce56137adcbff252a245ff4
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-01-14 19:52:45 -0600

    fix(projects): registered model Project in admin

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "organization", "key", "name", "is_active", "created_at")
+    search_fields = ("key", "name", "organization__slug")
+    list_filter = ("is_active", "organization")
