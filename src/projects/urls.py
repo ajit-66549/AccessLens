dfbbf9b87d912c85db7545448d241bb97616a2be
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-01-30 02:42:03 -0600

    feat(projects): complete retrieve, update, and delete endpoints

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectListCreateView
+from .views import ProjectListCreateView, ProjectDetailView
 
 urlpatterns = [
     path("", ProjectListCreateView.as_view(), name="projects"),
+    path("<uuid:project_id>/", ProjectDetailView.as_view(), name="project_details"),
 ]
--- src/projects/views.py
+++ src/projects/views.py
@@ -1,13 +1,14 @@
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
+
 from .models import Project
 from .serializers import ProjectSerializer
 from orgs.permissions import HasOrgMembership
-
-from django.db import IntegrityError
-from rest_framework.exceptions import ValidationError
-
 from audit.services import write_audit_event
 
 # Create your views here.
@@ -39,3 +40,60 @@ class ProjectListCreateView(APIView):
             return Response(ProjectSerializer(project).data, status=status.HTTP_201_CREATED)
         except IntegrityError:
             raise ValidationError({"key": "Project key must be unique within the organization"})
+        
+class ProjectDetailView(APIView):
+    # be the member of org to access the project
+    permission_classes = [HasOrgMembership]
+    
+    # get the project whose id is project_id and belongs to request.org
+    def get_object(self, request, project_id):
+        return get_object_or_404(Project, id = project_id, organization = request.org)
+    
+    # fetch the project whose id is project_id
+    def get(self, request, project_id):
+        project = self.get_object(request, project_id)
+        return Response(ProjectSerializer(project).data)
+    
+    # updates the whole project 
+    def put(self, request, project_id):
+        return self._update(request, project_id, partial=False)
+    
+    # updates the partial project
+    def patch(self, request, project_id):
+        return self._update(request, project_id, partial=True)
+    
+    def _update(self, request, project_id, *, partial):
+        project = self.get_object(request, project_id)
+        serializer = ProjectSerializer(project, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        try:
+            project = serializer.save()
+        except IntegrityError:
+            return ValidationError({"details": "Project key must be unique within the organization"})
+        
+        # audit the event
+        write_audit_event(
+            request=request, 
+            organization=request.org,
+            action="Project.updated",
+            target_type="Project",
+            target_id=project.id,
+            meta = {"key": project.key, "name": project.name},
+        )
+        return Response(ProjectSerializer(project).data)
+    
+    def delete(self, request, project_id):
+        project = self.get_object(request, project_id)
+        project_snap = {"key": project.key, "name": project.name},
+        project_id = project.id
+        project.delete()
+        
+        write_audit_event(
+            request=request,
+            organization=request.org,
+            action="Project.deleted",
+            target_type="Project",
+            target_id=project_id,
+            meta = project_snap,
+        )
+        return Response({"details": "Project Deleted"}, status=status.HTTP_204_NO_CONTENT)
