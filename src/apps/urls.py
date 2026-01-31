66becc18c97f4bb35254761d5d2784acc9edd3df
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-01-31 00:28:29 -0600

    feat(App): complete retrieve, update, delete endpoints for App

--- src/apps/urls.py
+++ src/apps/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AppListCreateView
+from .views import AppListCreateView, AppDetailView
 from .views_api_keys import CreateApiKeyView, ApiKeyPingView
 
 urlpatterns = [
     path("", AppListCreateView.as_view(), name="apps"),
     path("<uuid:app_id>/keys/", CreateApiKeyView.as_view(), name="app_create_key"),
     path("ping/", ApiKeyPingView.as_view(), name="api_view"),
+    path("<uuid:app_id>/", AppDetailView, name="app_details"),
 ]
--- src/apps/views.py
+++ src/apps/views.py
@@ -1,12 +1,15 @@
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
-from orgs.permissions import HasOrgMembership
-from projects.models import Project
 from .models import App
 from .serializers import AppSerializer
-
+from projects.models import Project
+from orgs.permissions import HasOrgMembership
 from audit.services import write_audit_event
 
 # Create your views here.
@@ -28,16 +31,78 @@ class AppListCreateView(APIView):
         if project.organization_id != request.org.id:
             return Response({"detail": "Project does not belong to this organization."}, status=status.HTTP_400_BAD_REQUEST)
         
-        app = App.objects.create(
+        try:
+            app = App.objects.create(
             organization=request.org,
             **serializer.validated_data,
-        )
+            )
+        except IntegrityError:
+            return ValidationError({"details": "App key must be unique within the project"})
+        
         # Audit log
         write_audit_event(request=request, 
                           organization=request.org,
                           action="app.created",
-                          target_type=" App",
+                          target_type="App",
                           target_id=app.id,
                           meta={"key": app.key, "name": app.name, "project_id": str(app.project_id)},)
         
         return Response(AppSerializer(app).data, status=status.HTTP_201_CREATED)
+    
+class AppDetailView(APIView):
+    permission_classes = [HasOrgMembership]
+    
+    def get_object(self, request, app_id):
+        return get_object_or_404(App, id=app_id, organization=request.org)
+    
+    def get(self, request, app_id):
+        app = get_object_or_404(request, app_id)
+        return Response(AppSerializer(app).data)
+    
+    def put(self, request, app_id):
+        return self._update(request, app_id, partial=False)
+    
+    def patch(self, request, app_id):
+        return self._update(request, app_id, partial=True)
+    
+    def _update(self, request, app_id, *, partial):
+        app = self.get_object(request, app_id)
+        serializer = AppSerializer(app, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        
+        if "project" in serializer.validated_data:
+            project = serializer.validated_data["project"]
+            if project.organization_id != request.org.id:
+                return Response({"details": "Project does not belong to this organization."},
+                                status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            app = serializer.save()
+        except IntegrityError:
+            return ValidationError({"details": "App key must be unique within the project"})
+        
+        write_audit_event(
+            request=request,
+            organization=request.org,
+            action="App.updated",
+            target_type="App",
+            target_id=app.id,
+            meta={"key": app.key, "name": app.name, "project_id": str(app.project_id)}
+        )
+        return Response(AppSerializer(app).data)
+    
+    def delete(self, request, app_id):
+        app = self.get_object(request, app_id)
+        app_snap = {"key": app.key, "name": app.name, "project_id": str(app.project_id)}
+        app_id = app.id
+        app.delete()
+        
+        write_audit_event(
+            request=request,
+            organization=request.org,
+            action="App.deleted",
+            target_type="App",
+            target_id=app.id,
+            meta=app_snap
+        )
+        return Response({"details": "App Deleted"}, status=status.HTTP_204_NO_CONTENT)
