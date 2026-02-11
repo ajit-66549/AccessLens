91b7de18d85daafc570a4214b12f334998ab09ac
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2026-02-11 00:45:37 -0600

    feat(api-keys): implement api key management (list, create, revoke, rotate) endpoints

--- src/apps/urls.py
+++ src/apps/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import AppListCreateView, AppDetailView
-from .views_api_keys import CreateApiKeyView, ApiKeyPingView
+from .views_api_keys import ApiKeyListCreateView, ApiKeyRevokeView, ApiKeyRotateView, ApiKeyPingView
 
 urlpatterns = [
     path("", AppListCreateView.as_view(), name="apps"),
-    path("<uuid:app_id>/keys/", CreateApiKeyView.as_view(), name="app_create_key"),
+    path("<uuid:app_id>/keys/", ApiKeyListCreateView.as_view(), name="list-create-key"),
+    path("<uuid:app_id>/keys/<uuid:key_id>/revoke/", ApiKeyRevokeView.as_view(), name="revoke-apikey"),
+    path("<uuid:app_id>/keys/<uuid:key_id>/rotate/", ApiKeyRotateView.as_view(), name="rotate-apikey"),
     path("ping/", ApiKeyPingView.as_view(), name="api_view"),
     path("<uuid:app_id>/", AppDetailView.as_view(), name="app_details"),
 ]
--- src/apps/views_api_keys.py
+++ src/apps/views_api_keys.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.utils import timezone
 
 from orgs.permissions import HasOrgMembership
 from rbac.permissions import IsOrgAdminOrOwner
@@ -10,14 +11,25 @@ from apps.authentication import AppApiKeyAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .permissions import HasAppApiKey
 
-class CreateApiKeyView(APIView):
+def SerializeApiKey(api_key: Apikey) -> dict:
+    return {
+        "id": str(api_key.id),
+        "name": api_key.name,
+        "app_id": str(api_key.app_id),
+        "is_active": api_key.is_active,
+        "created_at": api_key.created_at,
+        "revoked_at": api_key.revoked_at,
+        "created_by": str(api_key.created_by_id) if api_key.created_by_id else None,
+    }
+
+class ApiKeyListCreateView(APIView):
     """"
     Create an api key and return raw key
     """
     permission_classes = [IsOrgAdminOrOwner]
     
-    def post(self, request, app_id):
-        # get that app
+    # give the app with the given id 
+    def get_app(self, request, app_id):
         try:
             app = App.objects.select_related("project__organization").get(id=app_id)
         except App.DoesNotExist:
@@ -26,6 +38,23 @@ class CreateApiKeyView(APIView):
         # check if the app is in this organization
         if app.project.organization_id != request.org.id:
             return Response({"detail": "App is not in this organization"}, status=status.HTTP_403_FORBIDDEN)
+        
+        return app, None
+    
+    # lists all the api_keys in the app
+    def get(self, request, app_id):
+        apps, error_message = self.get_app(request, app_id)
+        
+        if error_message:
+            return error_message
+        
+        api_keys = Apikey.objects.filter(app=apps).select_related("created_by").order_by("-created.at")
+        return Response([SerializeApiKey(api_key) for api_key in api_keys])
+    
+    def post(self, request, app_id):
+        app, error_message = self.get_app(request, app_id)
+        if error_message:
+            return error_message
         
         name = request.data.get("name", "").strip()
         
@@ -52,7 +81,77 @@ class CreateApiKeyView(APIView):
                 "raw_key": raw_key,
             }, status=status.HTTP_201_CREATED,
         )
+
+# disable the api_key
+class ApiKeyRevokeView(APIView):
+    permission_classes = [IsOrgAdminOrOwner]
+    
+    def post(self, request, app_id, key_id):
+        try:
+            app = App.objects.select_related("project__organization").get(id=app_id)
+        except App.DoesNotExist:
+            return Response({"detail": "App Does Not Exist"}, status=status.HTTP_404_NOT_FOUND)
         
+        if app.project.organization_id != request.org.id:
+            return Response({"detail": "App does not delong to this organization"}, status=status.HTTP_403_FORBIDDEN)
+        
+        # get the api key with thegiven key id in the given app
+        api_key = Apikey.objects.filter(app=app, id=key_id).first()
+        
+        # if no api_key, return message
+        if not api_key:
+            return Response({"detail": "Api Key Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        # check if the api_key is already revoked
+        if api_key.revoked_at:
+            return Response({"deatil": "Api Key is already revoked", "api_key": SerializeApiKey(api_key)}, status=status.HTTP_200_OK)
+        
+        # revoke the api key
+        api_key.is_active = False
+        api_key.revoked_at = timezone.now() 
+        api_key.save(update_fields=["is_active", "revoked_at"])
+        
+        return Response({"detail": "Apikey revoked.", "api_key": SerializeApiKey(api_key)}, status=status.HTTP_200_OK)
+        
+# creates a new api_key for the app, and revokes the old one
+class ApiKeyRotateView(APIView):
+    permission_classes = [IsOrgAdminOrOwner]
+    
+    def post(self, request, app_id, key_id):
+        try:
+            app = App.objects.select_related("project__organization").get(app=app_id)
+        except App.DoesNotExist:
+            return Response({"deatil": "App does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if app.project.organization_id != request.org.id:
+            return Response({"detail": "App doesn't belong to this organization"}, status=status.HTTP_403_FORBIDDEN)
+        
+        # get the old api key 
+        old_key = Apikey.objects.filter(app=app, id=key_id).first()
+        if not old_key:
+            return Response({"detail": "Api Key Not Found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if old_key.revoked_at:
+            return Response({"deatil": "Api Key is already revoked"}, status=status.HTTP_200_OK)
+        
+        name = request.data.get("name", "").strip() or old_key.name
+        
+        raw_key = Apikey.generate_api_key()
+        hash_key = Apikey.hash_key(raw_key)
+        
+        new_key = Apikey.objects.create(app=app, key_hash=hash_key, name=name, created_by=request.user if request.user.is_authenticated else None,)
+        
+        old_key.is_active = False
+        old_key.revoked_at = timezone.now()
+        old_key.save(update_fields=["is_active", "revoked_at"])
+        
+        return Response({
+            "detail": "Api key rotated",
+            "raw_key": raw_key,
+            "api_key": SerializeApiKey(new_key),
+            "revoked_key": SerializeApiKey(old_key),
+            }, status=status.HTTP_201_CREATED)
+
 class ApiKeyPingView(APIView):
     authentication_classes = [AppApiKeyAuthentication]
     permission_classes = [HasAppApiKey]
